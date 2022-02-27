5bf4f35fefe91eed4dbc48caa01c929295875310
rishigarg256 <49986448+rishigarg256@users.noreply.github.com>
2022-02-26 20:17:29 -0500

    Update deploy.py
    private key had missing 0x. also, over here, why not using os.getenv for private key instead of having the private key over her?

--- deploy.py
+++ deploy.py
@@ -54,7 +54,7 @@ abi = json.loads(
 w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
 chain_id = 1337
 my_address = "0xdbB4A708755dfD59f9c4b100B2BE23a6d2EB7D57"
-private_key = "ffdd7a010ab8c089d95a9c2ff24e75b21744b5db26c3cd66d14f8e91c46afcc4"
+private_key = "0xffdd7a010ab8c089d95a9c2ff24e75b21744b5db26c3cd66d14f8e91c46afcc4"
 
 # Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
